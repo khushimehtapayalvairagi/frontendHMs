a77ccf7f005535a571b5d72377e14b00cbb8977b
Khushi Mehta <khushimehta82339@gmail.com>
2026-04-10 00:50:50 +0530

    initial backend commit

--- src/components/Dashboard/IPDAdmissionList.js
+++ src/components/Dashboard/IPDAdmissionList.js
@@ -11,7 +11,7 @@ const location = useLocation();
 const [patientName, setPatientName] = useState(location.state?.patientName || '');
 
   const [admissions, setAdmissions] = useState([]);
-
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
 
   useEffect(() => {
     if (!patientId) return;
@@ -19,9 +19,10 @@ const [patientName, setPatientName] = useState(location.state?.patientName || ''
     async function fetchAdmissions() {
       try {
         const res = await axios.get(
-          `http://localhost:8000/api/ipd/admissions/${patientId}`,
+          `${BASE_URL}/api/ipd/admissions/${patientId}`,
           { headers: { Authorization: `Bearer ${token}` } }
         );
+
         console.log("Fetching admissions for:", patientId);
         setAdmissions(res.data.admissions || []);
       } catch (err) {
