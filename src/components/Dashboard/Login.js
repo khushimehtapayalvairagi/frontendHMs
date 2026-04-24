26b185f6f003847200258f71460a2e0ab08e9ee5
Khushi Mehta <khushimehta82339@gmail.com>
2026-04-24 23:58:54 +0530

    initial backend commit

--- src/components/Dashboard/Login.js
+++ src/components/Dashboard/Login.js
@@ -46,8 +46,9 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
     if (user.role === "ADMIN") navigate("/admin-dashboard");
     else if (user.designation === "Receptionist") navigate("/receptionist-dashboard");
     else if (user.role === "DOCTOR") navigate("/doctor-dashboard");
-    else if (user.designation === "Head Nurse") navigate("/nurse-dashboard");
-    else if (user.designation === "Inventory Manager") navigate("/inventoryManager-dashboard");
+    else if (user.designation === "Metron") navigate("/nurse-dashboard");
+    else if (user.designation === "O.T. Attendant") navigate("/nurse-dashboard");
+    else if (user.designation === "Pharmacists") navigate("/inventoryManager-dashboard");
     else toast.error("Unknown role, cannot navigate", { position: "top-center", autoClose: 4000 });
 
   } catch (err) {
